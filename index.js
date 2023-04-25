21f4f716a1ba2190782024f27bf55d0666490ff2
Ahmed Khalifa <88500869+IamAhmedKhalifa@users.noreply.github.com>
2023-04-25 15:13:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-let totalEl = document.getElementById("total-el")
 let count = 0
 
 function increment() {
@@ -14,5 +13,4 @@ function save() {
     countEl.textContent = 0
     count = 0
     countEl.textContent = count
-    totalEl.textContent = count
 }
